9c1cd78fcbd951cc5c094afde7e44e335255e295
Jose Moya <jose_moya@live.com.ar>
2024-04-12 23:49:40 -0300

    refactorizacion

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,15 @@
 import Sequelize from "sequelize";
 import "dotenv/config";
 
-export const sequelize = new Sequelize("tmdb_db", "joseMoya", "db_password", {
-  host: "database",
-  dialect: "postgres",
-});
+const { PORT, POSTGRES_HOST, POSTGRES_DB, POSTGRES_USER, POSTGRES_PASSWORD } =
+  process.env;
+
+export const sequelize = new Sequelize(
+  POSTGRES_DB,
+  POSTGRES_USER,
+  POSTGRES_PASSWORD,
+  {
+    host: POSTGRES_HOST,
+    dialect: "postgres",
+  }
+);
--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import app from "./app.js";
 import "./models/index.js";
 
 async function main() {
-  const PORT = process.env.PORT;
+  const { PORT } = process.env;
   const MESSAGE = process.env.SERVER_MESSAGE;
   try {
     await sequelize.sync({ force: false });
